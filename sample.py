c72bf62136c043c4e2013902dfa937f64f7a38ec
psankar6 <60560407+psankar6@users.noreply.github.com>
2025-02-13 00:54:05 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,8 @@
 # this is sample python file
-print("hello")
+print("hello world")
 str="sample"
 print(str)
-str2="Ranjith"
+str2="Ranjith kumar subramanian"
 print("My name is:",str2)
 str1="I am from salem"
 print(str1)
